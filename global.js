52875e9a727f83d146d0a494bf24bf193ebd734e
The0eau <theauaguetpro@gmail.com>
2025-01-24 09:20:06 -0800

    Lab 3

--- global.js
+++ global.js
@@ -12,3 +12,15 @@ let currentLink = navLinks.find(
   );
 
 currentLink?.classList.add('current');
+
+let pages = [
+    { url: 'https://the0eau.github.io/portfolio/index.html', title: 'Me' },
+    { url: 'https://the0eau.github.io/portfolio/project/index.html', title: 'Projects' },
+    { url: 'https://the0eau.github.io/portfolio/contact/index.html', title: 'Contact' },
+    { url: 'https://the0eau.github.io/portfolio/cv/index.html', title: 'Resume' },
+    { url: 'https://github.com/The0eau', title: 'Github' },
+    // add the rest of your pages here
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
