897545ed7eeccf3508c3de75af371e1fc4bb1614
The0eau <theauaguetpro@gmail.com>
2025-01-24 09:32:21 -0800

    Lab 3

--- global.js
+++ global.js
@@ -36,3 +36,19 @@ for (let p of pages) {
 
 // Create link and add it to nav
 nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+let a = document.createElement('a');
+a.href = url;
+a.textContent = title;
+nav.append(a);
+if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+  
+a.classList.toggle(
+'current',
+a.host === location.host && a.pathname === location.pathname
+);
