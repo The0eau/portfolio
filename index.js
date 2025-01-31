7ec2e0cf6f86ad7f8c4539bfc079adfde615e4a1
The0eau <theauaguetpro@gmail.com>
2025-01-31 14:23:14 -0800

    Lab 4

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { fetchJSON, renderProjects, fetchGithubData } from 'https://raw.githubusercontent.com/The0eau/portfolio/refs/heads/main/global.js';
-const projects = await fetchJSON('https://raw.githubusercontent.com/The0eau/portfolio/refs/heads/main/lib/projects.json');
+import { fetchJSON, renderProjects, fetchGithubData } from 'https://the0eau.github.io/portfolio/global.js';
+const projects = await fetchJSON('https://the0eau.github.io/portfolio/projects.json');
 const latestProjects = projects.slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
