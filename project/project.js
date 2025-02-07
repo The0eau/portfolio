850cce7f424192dd2b06696b2f73214aca9b5053
The0eau <theauaguetpro@gmail.com>
2025-02-06 18:53:04 -0800

    Lab 5

--- project/project.js
+++ project/project.js
@@ -11,9 +11,19 @@ document.querySelector(".projects-title").textContent = `${projectCount} project
 
 
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
+
+
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let data = [1, 2, 3, 4, 5, 5];
-let sliceGenerator = d3.pie();
+
+let data = [
+    { value: 1, label: 'apples' },
+    { value: 2, label: 'oranges' },
+    { value: 3, label: 'mangos' },
+    { value: 4, label: 'pears' },
+    { value: 5, label: 'limes' },
+    { value: 5, label: 'cherries' },
+];
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
@@ -24,4 +34,11 @@ arcs.forEach((arc, i) => {
     .attr('d', arc)
     .attr("fill", colors(i))
   })
+
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+    legend.append('li')
+          .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+})
 
