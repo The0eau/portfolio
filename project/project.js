b96e09a792f9448b49024066f2f9a671b1d2a59e
The0eau <theauaguetpro@gmail.com>
2025-02-06 18:20:48 -0800

    Lab 5

--- project/project.js
+++ project/project.js
@@ -7,3 +7,20 @@ renderProjects(projects, projectsContainer, 'h2');
 const projectCount = document.querySelectorAll(".projects article").length;
 
 document.querySelector(".projects-title").textContent = `${projectCount} projects`;
+
+
+
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
+let data = [1, 2, 3, 4, 5, 5];
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+arcs.forEach(arc => {
+    // TODO, fill in step for appending path to svg using D3
+    d3.select('svg')
+    .append('path')
+    .attr('d', arc)
+    .attr("fill", (d, i) => colors(i))
+  })
+
