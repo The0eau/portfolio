5bc1fe26e15deec9d8b40678a1dda299957bb996
The0eau <theauaguetpro@gmail.com>
2025-02-07 09:24:11 -0800

    Lab 5

--- project/project.js
+++ project/project.js
@@ -40,5 +40,7 @@ data.forEach((d, idx) => {
     legend.append('li')
           .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
           .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+           .attr("class", "legend-item")
+    .attr(`--color:${colors(idx)}`)
 })
 
