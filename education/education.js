e81b17534a50dc64da119de7cb4b827eebcc88c5
The0eau <theauaguetpro@gmail.com>
2025-02-13 19:44:50 -0800

    Lab 6

--- education/education.js
+++ education/education.js
@@ -1,5 +1,6 @@
 import {fetchJSON} from 'https://the0eau.github.io/portfolio/global.js';
 const educations = await fetchJSON('https://raw.githubusercontent.com/The0eau/portfolio/main/lib/education.json');
+const courses = await fetchJSON('https://raw.githubusercontent.com/The0eau/portfolio/main/lib/education.json');
 function renderEducation(education, containerElement, headingLevel = 'h2') {
     // Your code will go here
     containerElement.innerHTML = '';
@@ -16,6 +17,8 @@ function renderEducation(education, containerElement, headingLevel = 'h2') {
       containerElement.appendChild(article);
   });
   }
-  const educationsContainer = document.querySelector('.educations');
+const educationsContainer = document.querySelector('.educations');
+const coursesContainer = document.querySelector('.courses');
 
-renderEducation(educations,educationsContainer,'h2')
+renderEducation(educations,educationsContainer,'h2');
+renderEducation(courses,coursesContainer,'h2')
